25265fe51193c57273279622b064c7345a724ee2
Sincab <sinan.selcuk@hotmail.com>
2020-02-19 20:06:59 +0000

    Youtube Corey Session 2

--- yout-02.py
+++ yout-02.py
@@ -6,8 +6,13 @@ message_1 = greeting + ', ' + name + '. Welcome!'
 message_2 = '{}, {}. Welcome!'.format(greeting, name)
 
 # f strings option
-message_3 = f'{greeting}, {name}. Welcome!'
+message_3 = f'{greeting}, {name.upper()}. Welcome!'
 
 print(message_1)
 print(message_2)
 print(message_3)
+
+# help stuff
+print(dir(name)) # overall options
+print(help(str)) # more details
+print(help(str.lower)) # further details
