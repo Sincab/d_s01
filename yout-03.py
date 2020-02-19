643af98141608bede9e8d4dbfbf0b90117f2889f
Sincab <sinan.selcuk@hotmail.com>
2020-02-19 21:32:45 +0000

    Youtube Corey Session 2

---
+++ yout-03.py
@@ -0,0 +1,24 @@
+# floor division // ---- 3 // 2 = 1
+# exponent ** ---- 3 ** 2 = 9
+# modulus %  ----- 5 % 3 = 2
+# equal 3 == 2
+# not equal 3 != 2
+# greater or equal 3 >= 2
+# smaller or equal 3 <= 2
+num_1 = 3 # int
+num_2 = 3.5 # float
+num = 1
+num = num + 2
+print(num)
+num = 1
+num **= 2
+print(num)
+print(abs(-7))
+print(round(3.7589, 2))
+
+print(num_1 != num_2)
+
+num = '100'
+print(int(num) / 2) # convert it to number
+
+
