a1fc097d34131a189f461210c37e15c85ca05ab5
Sincab <sinan.selcuk@hotmail.com>
2020-02-24 12:32:48 +0000

    Youtube Corey Session 4 and 5 Lists, Tuples, Sets and Dictionaries

--- List_set_tupple_dict.py
+++ List_set_tupple_dict.py
@@ -37,3 +37,43 @@ print(type(empty_set))
 print(' -------  dictionary-----')
 
 student = {'name' : 'John', 'age' : 25, 'courses' : ['Math', 'CompSci']}
+print(student['name']) # get is better as if key not in the dic, it will fail, get will return none
+print(student.get('name'))
+print(student.get('phone', 'Not Found'))
+
+#adding new key
+
+student['phone'] = '555-55545'
+print(student.get('phone'))
+#updating 1
+student['name'] = 'Sinan'
+print(student.get('name'))
+#updating 2 - useful for multiple
+student.update({'name': 'Helene', 'age': 34, 'phone': '545465'})
+print(student)
+
+#deleting - delete and pop method - pop allows to capture poped out info
+
+del student['age']
+print(student)
+phone = student.pop('phone')
+print(student)
+print(phone)
+print('===== len and counting looping ====')
+student = {'name' : 'John', 'age' : 25, 'courses' : ['Math', 'CompSci']}
+student.update({'name': 'Helene', 'age': 34, 'phone': '545465'})
+print(student)
+print(len(student))
+print(student.keys())
+print(student.values())
+print(student.items())
+
+for key in student:
+    print(key)
+
+for key, values in student.items():
+    print(key, values)
+
+
+
+
