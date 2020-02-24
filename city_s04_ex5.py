fa981b3bf5194314927c82b246dc2dcafb6e43f8
Sincab <sinan.selcuk@hotmail.com>
2020-02-24 19:11:13 +0000

    City wk04 ex 05 small mod

--- city_s04_ex5.py
+++ city_s04_ex5.py
@@ -2,16 +2,17 @@ def main():
     entered_word_1 = input('enter first word:')
     entered_word_2 = input('enter second word:')
 
-    print('They are ', isAnagram(entered_word_1, entered_word_2))
+    print('They are ', is_anagram(entered_word_1, entered_word_2))
 
 
-
-def isAnagram(entered_word_1, entered_word_2):
+def is_anagram(entered_word_1, entered_word_2):
     if sorting_thins(entered_word_1) == sorting_thins(entered_word_2):
-        return 'Anagram'
-    return 'NOT Angram'
+        return 'Anagram.'
+    return 'NOT Anagram.'
+
 
 def sorting_thins(string):
     return sorted([s for s in string])
+
 
 main()
